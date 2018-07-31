3f56e7db333d7eb16ec19db3ffe4a16bf30f1e44
David whyte <kingstonwhyte87@gmail.com>
2018-07-31 06:32:06 -0700

    v4
    Fragments of sidenav is funcioning well

--- app/src/main/java/com/example/davidwhyte/safehouse/Dashboard.kt
+++ app/src/main/java/com/example/davidwhyte/safehouse/Dashboard.kt
@@ -1,4 +1,10 @@
 package com.example.davidwhyte.safehouse
 
-class Dashboard {
+import android.os.Bundle
+import android.support.v4.app.Fragment
+
+class Dashboard:Fragment(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
--- app/src/main/java/com/example/davidwhyte/safehouse/MainActivity.kt
+++ app/src/main/java/com/example/davidwhyte/safehouse/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBar
@@ -29,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
 
         val navigationView: NavigationView = findViewById(R.id.nav_view)
+        displayFragment(0)
         navigationView.setNavigationItemSelectedListener { menuItem ->
             // set item as selected to persist highlight
             menuItem.isChecked = true
@@ -37,7 +40,7 @@ class MainActivity : AppCompatActivity() {
 
             // Add code here to update the UI based on the item selected
             // For example, swap UI fragments here
-
+            displayFragment(menuItem.itemId)
             true
         }
     }
@@ -49,12 +52,29 @@ class MainActivity : AppCompatActivity() {
                 Log.v("home","95")
                 true
             }
-            R.id.nav_dash->{
-                Log.v("home","95")
-                true
-            }
             else -> super.onOptionsItemSelected(item)
         }
+
+    }
+    fun displayFragment(item_id:Int){
+        var fragment:Fragment?=null
+        if(item_id==R.id.nav_dash){
+            fragment=DashFragment()
+        }
+        if(item_id==0){
+            fragment=DashFragment()
+        }
+        if(item_id==R.id.nav_profile){
+            fragment=ProfileFragment()
+        }
+
+        if(fragment!=null){
+            Log.v("loaded_f","yes")
+            val transaction:FragmentTransaction=supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.content_frame,fragment)
+            transaction.commit()
+        }
+
     }
 
 
